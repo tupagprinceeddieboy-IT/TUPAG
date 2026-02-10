b5f87dff6cacb7bc420399ccb43a189d9e5da98c
tupagprinceeddieboy-IT <tupag.princeeddieboy@gmail.com>
2026-02-10 14:07:00 +0800

    pree

---
+++ 2nd.py
@@ -0,0 +1,6 @@
+score = 73
+
+if score >75:
+     print("You Passed")
+else:
+     print("You Failed")
---
+++ 3rd.py
@@ -0,0 +1,7 @@
+score = 89
+
+if score >75:
+     print("You Passed")
+else:
+     print("You Failed")
+
