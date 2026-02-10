5515dcdbc9c8e29efe66d44b435b9c715986c256
tupagprinceeddieboy-IT <tupag.princeeddieboy@gmail.com>
2026-02-10 14:01:39 +0800

    PRINCE.TUPAG

---
+++ main.py
@@ -0,0 +1,16 @@
+name= "Prince"
+age= 19
+height=1.39
+is_working=False
+
+print("fullname:", name)
+print(type(name))
+
+print("\nAge", age)
+print(type(age))
+
+print("\nHeight:, Height")
+print(type(height))
+
+print("\nIs Working;", is_working)
+print(type(is_working))
